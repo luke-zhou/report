fe2001c11e9cad3f591ac22267f6b2020c052c8d
Luke <sharlock@gmail.com>
2015-03-12 22:20:48 +1100

    init table and basic style

--- app/app.js
+++ app/app.js
@@ -2,5 +2,35 @@
 
 // Declare app level module which depends on views, and components
 (function(){
-	angular.module('report', []);
+	var app = angular.module('report', []);
+
+	app.controller('reportController', function(){
+		this.rows = participants;
+	})
+
+	var participants=[
+	{
+		name: 'candidate one',
+		video: 34,
+		cognitive: 56,
+		behavior: 78,
+		overall: 65
+	},
+	{
+		name: 'candidate two',
+		video: 43,
+		cognitive: 65,
+		behavior: 87,
+		overall: 79
+	},
+	{
+		name: 'candidate three',
+		video: 98,
+		cognitive: 23,
+		behavior: 76,
+		overall: 60
+	}
+	]
+
+
 })();
