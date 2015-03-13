09ed76aebf6e8bb5fa317b370e074589e470c122
Luke <luke.zhou@testgrid.com>
2015-03-13 13:47:10 +1100

    update model

--- app/app.js
+++ app/app.js
@@ -6,31 +6,88 @@
 
 	app.controller('reportController', function(){
 		this.rows = participants;
+		this.keys = [];
+		for (var key in this.rows[0].scores) {
+		  if (this.rows[0].scores.hasOwnProperty(key)) {
+			this.keys.push(key);
+			console.log(key);
+		  }
+		}
 	})
 
 	var participants=[
 	{
 		name: 'candidate one',
-		video: 34,
-		cognitive: 56,
-		behavior: 78,
+		scores:{
+			video: {
+				question1:27,
+				question2:56,
+				question3:30,
+				overall:34
+			},
+			cognitive: {
+				verbal:89,
+				numerical:56,
+				abstract:30,
+				overall:56
+			},
+			behavior: {
+				competency1:20,
+				competency2:98,
+				competency3:10,
+				overall:85
+			}
+		},
 		overall: 65
 	},
 	{
 		name: 'candidate two',
-		video: 43,
-		cognitive: 65,
-		behavior: 87,
+		scores:{
+			video: {
+				question1:15,
+				question2:86,
+				question3:40,
+				overall:43
+			},
+			cognitive: {
+				verbal:56,
+				numerical:91,
+				abstract:38,
+				overall:65
+			},
+			behavior: {
+				competency1:86,
+				competency2:88,
+				competency3:89,
+				overall:87
+			},
+		},
 		overall: 79
 	},
 	{
 		name: 'candidate three',
-		video: 98,
-		cognitive: 23,
-		behavior: 76,
+		scores:{
+			video: {
+				question1:65,
+				question2:99,
+				question3:99,
+				overall:98
+			},
+			cognitive: {
+				verbal:50,
+				numerical:11,
+				abstract:10,
+				overall:23
+			},
+			behavior: {
+				competency1:84,
+				competency2:98,
+				competency3:62,
+				overall:76
+			},
+		},
 		overall: 60
 	}
 	]
-
 
 })();
