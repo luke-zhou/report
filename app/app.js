c6217fd391d5be27245c9c16ef20c3ddfccdf67a
Luke <sharlock@gmail.com>
2015-03-23 14:22:18 +1100

    update chart and add more data

--- app/app.js
+++ app/app.js
@@ -38,25 +38,66 @@
 			this.selectAssessmentDatailTable=true;
 			
 			this.selectAssessmentKey= key;
-		}
+		};
 		
 		this.showMainTable = function() {
 			this.selectMainTable=true;
 			this.selectParticipantDetailTable=false;
 			this.selectAssessmentDatailTable=false;		
-		}
+		};
 		
-		this.updateChart = function(score){
-		var data = 
-			{
-				min:4,
-				max:96,
-				avg:65
+		this.updateChart = function(participant, scoreKey, competency){
+		console.log(participant.scores[scoreKey][competency]);
+
+		var data = prepareChartData(participant, scoreKey, competency);
+
+		updateChart(data);
+		};
+
+		var prepareChartData= function(participant, scoreKey, competency){
+	
+			var data = {};
+			data.score = participant.scores[scoreKey][competency];
+			data.min = getMinScore(scoreKey, competency);
+			data.max = getMaxScore(scoreKey, competency);
+			data.avg = getAverageScore(scoreKey, competency);
+			
+			return data;
+		};
+		
+		var getMinScore = function(scoreKey, competency){
+			var min = 100;
+			for (var index in participants){
+				var participant = participants[index];
+				if (participant.scores[scoreKey][competency]<min){
+					min = participant.scores[scoreKey][competency];
+				};
 			};
-			data.score = score;
-			updateChart(data);
-		}
+			return min;
+		};
 		
+		var getMaxScore = function(scoreKey, competency){
+			var max = 0;
+			for (var index in participants){
+				var participant = participants[index];
+				if (participant.scores[scoreKey][competency]>max){
+					max = participant.scores[scoreKey][competency];
+				};
+			};
+			return max;
+		};
+
+		var getAverageScore = function(scoreKey, competency){
+			var sum = 0;
+			var count = 0;
+			for (var index in participants){
+				var participant = participants[index];
+					sum += participant.scores[scoreKey][competency];
+					count++;
+				};
+			return Math.round(sum/count);
+		};
+
 		var getParticipant = function(participantId){
 			console.log("participants:"+participants);
 			for (var index in participants){
@@ -66,92 +107,5 @@
 		};
 		
 	});
-
-	var participants=[
-	{
-		id: 1,
-		name: 'candidate one',
-		scores:{
-			video: {
-				question1:27,
-				question2:56,
-				question3:30,
-				question4:35,
-				overall:34
-			},
-			cognitive: {
-				verbal:89,
-				numerical:56,
-				abstract:30,
-				overall:56
-			},
-			behavior: {
-				competency1:20,
-				competency2:98,
-				competency3:10,
-				competency4:15,
-				competency5:50,
-				overall:85
-			}
-		},
-		overall: 65
-	},
-	{
-		id: 2,
-		name: 'candidate two',
-		scores:{
-			video: {
-				question1:15,
-				question2:86,
-				question3:40,
-				question4:45,
-				overall:43
-			},
-			cognitive: {
-				verbal:56,
-				numerical:91,
-				abstract:38,
-				overall:65
-			},
-			behavior: {
-				competency1:86,
-				competency2:88,
-				competency3:89,
-				competency4:94,
-				competency5:45,
-				overall:87
-			},
-		},
-		overall: 79
-	},
-	{
-		id: 3,
-		name: 'candidate three',
-		scores:{
-			video: {
-				question1:65,
-				question2:99,
-				question3:99,
-				question4:90,
-				overall:98
-			},
-			cognitive: {
-				verbal:50,
-				numerical:11,
-				abstract:10,
-				overall:23
-			},
-			behavior: {
-				competency1:84,
-				competency2:98,
-				competency3:62,
-				competency4:81,
-				competency5:32,
-				overall:76
-			},
-		},
-		overall: 60
-	}
-	];
 
 })();
--- app/chart.js
+++ app/chart.js
@@ -6,19 +6,24 @@ var chart = d3.select(".chart");
 chart.select("div.background").style("width",  99* 5 + "px");
 chart.select("div.score-range").style("width",  (data.max-data.min)* 5 + "px").style("height", "25px").style("left",(data.min)* 5 + "px");
 
+if(data.avg<data.score){
+var avgTop = '-20px';
+var scoreTop = '25px';
+}
+else{
+var avgTop = '25px';
+var scoreTop = '-20px';
+}
+
 //min measurement
-chart.select("div.min-text").text("Min").style("left",(data.min)* 5-10 + "px");
-chart.select("div.min-num").text(data.min).style("left",(data.min)* 5-5 + "px");
+chart.select("div.min-text").text("Min("+data.min+")").style("left",(data.min)* 5-25 + "px");
 
 //average bar
 chart.select("div.avg-bar").style("width", "3px").style("left",(data.avg)* 5 + "px");
-chart.select("div.avg-text").text("Avg").style("left",(data.avg)* 5-10 + "px");
-chart.select("div.avg-num").text(data.avg).style("left",(data.avg)* 5-7 + "px");
+chart.select("div.avg-text").text("Avg("+data.avg+")").style("left",(data.avg)* 5-25 + "px").style("top",avgTop);
 
 chart.select("div.score-bar").style("width", "3px").style("left",(data.score)* 5 + "px");
-chart.select("div.score-text").text("Score").style("left",(data.score)* 5-17 + "px");
-chart.select("div.score-num").text(data.score).style("left",(data.score)* 5-7 + "px");
+chart.select("div.score-text").text("Score("+data.score+")").style("left",(data.score)* 5-30 + "px").style("top",scoreTop);
 
-chart.select("div.max-text").text("Max").style("left",(data.max)* 5-15 + "px");
-chart.select("div.max-num").text(data.max).style("left",(data.max)* 5-7 + "px");
+chart.select("div.max-text").text("Max("+data.max+")").style("left",(data.max)* 5-25 + "px");
 };
