a53467b455f99bdabd582b937b39435c0edb28ae
Luke <sharlock@gmail.com>
2015-07-02 22:53:13 +1000

    add mapping project

---
+++ mapping/mapping.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+(function(){
+	var app = angular.module('mapping', []);
+
+	app.controller('mappingController', function(){
+
+		
+	});
+
+})();
