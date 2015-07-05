167258edab6b4c7439684c3f7c512c65bad88ddf
Luke <sharlock@gmail.com>
2015-07-05 19:35:49 +1000

    clean css class

--- mapping/js/mapping.js
+++ mapping/js/mapping.js
@@ -7,9 +7,14 @@
 	app.controller('mappingController', function(){
 		//this.nodeGroupName="test";
 		this.nodes=[];
+
 		this.addNodes = function(){
 			this.nodes.push({name:this.nodeName, code: this.nodeCode, type:this.nodeType, order:this.nodeOrder, parentNode:this.parentNode, rank:this.nodeRank, weight:this.nodeWeight, description:this.nodeDescription});
-		}
+		};
+
+		this.updateCode = function(){
+			this.nodeCode = this.nodeName.toUpperCase().replace(/ /g, "_");
+		};
 	});
 
 })();
