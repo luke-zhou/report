da72c5da8fde6496e47c4c583857c08b72a6d9a0
Luke <sharlock@gmail.com>
2015-07-13 17:17:20 +1000

    add orders list

--- mapping/js/mapping.js
+++ mapping/js/mapping.js
@@ -7,9 +7,18 @@
 	app.controller('mappingController', function(){
 		//this.nodeGroupName="test";
 		this.nodes=[];
-
+		this.orders=[];
+		
+		var generateNumber = function(value, index){
+			return Number(index+1);
+		};
+		this.updateOrderList = function(){
+			this.orders = Array.apply(null, {length:this.nodes.length}).map(generateNumber, Number);
+		};
+		
 		this.addNodes = function(){
 			this.nodes.push({name:this.nodeName, code: this.nodeCode, type:this.nodeType, order:this.nodeOrder, parentNode:this.parentNode, rank:this.nodeRank, weight:this.nodeWeight, description:this.nodeDescription});
+			this.updateOrderList();
 		};
 
 		this.updateCode = function(){
